c058ea33c8f4b07348779d62968666b0b986e823
CTEPXMGN <fuser-mgn@ya.ru>
2025-09-22 19:35:49 +0500

    2

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,46 @@
-import Swiper from '../vendor/swiper/swiper-bundle.min.mjs'
-import { themeSwitcher } from '../vendor/theme-switcher/theme-switcher.js'
-new themeSwitcher()
+import Swiper from "../vendor/swiper/swiper-bundle.min.mjs";
+import { themeSwitcher } from "../vendor/theme-switcher/theme-switcher.js";
+new themeSwitcher();
 
-const menuBtn = document.querySelector('.menu-btn')
-const menu = document.querySelector('.main-menu ul')
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".main-menu ul");
 
-menuBtn.addEventListener('click', function() {
-  this.classList.toggle('active')
-  menu.classList.toggle('active')
-})
+menuBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  menu.classList.toggle("active");
+});
 
-const swiper = new Swiper('.head__slider', {
-  effect: 'slide',
+const swiper = new Swiper(".head-slider", {
+  effect: "slide",
   speed: 1000,
   loop: true,
   slidesPerView: 3,
   spaceBetween: 30,
   mousewheel: true,
-  // pagination: {
-  //   el: '.swiper-pagination',
-  //   clickable: true
-  // },
-  // breakpoints: {
-  //   0: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 20
-  //   },
-  //   768: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 30
-  //   }
-  // }
-})
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  on: {
+    init: function () {
+      updateSlideCounter(this);
+    },
+    slideChange: function () {
+      updateSlideCounter(this);
+    },
+  },
+});
+
+function updateSlideCounter(swiperInstance) {
+  const realIndex = swiperInstance.realIndex + 1;
+  console.log(realIndex);
+  
+  const totalSlides = swiperInstance.slides.length;
+  const counterElement = document.querySelector(".slider-bottom__count .value");
+
+  if (counterElement) {
+    counterElement.textContent = `${realIndex} / ${totalSlides}`;
+  } else {
+    console.warn("Элемент .slider-bottom__count .value не найден");
+  }
+}
